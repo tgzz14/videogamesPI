42a376d5ef6e4f9f9ad9cdcd584aa51ce7369aad
tgzz14 <108690992+tgzz14@users.noreply.github.com>
2023-04-07 08:34:14 -0600

    test

--- client/src/components/LandingPage/Landing.jsx
+++ client/src/components/LandingPage/Landing.jsx
@@ -1,18 +1,8 @@
-//import React, { useEffect } from 'react';
-//import { useDispatch } from 'react-redux';
-//import { getVideogames, setLoading } from '../../redux/action.js';
 import { useNavigate } from 'react-router-dom';
 import './Lading.css'
 
-
 export default function Landing() {
-   //const dispatch = useDispatch()
     const navigate = useNavigate()
-//     useEffect(()=>{
-//         dispatch(getVideogames()).then(() => dispatch(setLoading(false)))
-// //eslint-disable-next-line react-hooks/exhaustive-deps
-// },[]) 
-
  
     return(
         <div className='container-landing'>
